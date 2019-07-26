9569c1105a8da916983a52de51fcdce9a00d4744
budihartantom <budihartantom@gmail.com>
2019-07-27 04:06:51 +0700

    modified today

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -9,7 +9,7 @@
 	<meta name="description" content="">
 	<meta name="author" content="">
 
-	<title>Halaman Administrator</title>
+	<title>Toko Online</title>
 
 	<!-- Custom fonts for this template-->
 	<link href="<?php echo base_url() ?>assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
